eeb9ba91baff9b0ad44083d6e3e5268182b90627
Bouzgaou <youcefbouzgaou@gmail.com>
2020-03-12 22:16:21 +0100

    Level design improvement

---
+++ boi/Assets/Ressources/Generic Scripts/MovingPlateform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlateform : MonoBehaviour
+{
+    public GameObject player;
+    public bool isOnPlat;
+
+    private void Update()
+    {
+        MovePlat();
+    }
+                               //Fonctionnement
+    // j'initialise un raycast qui check le tag de la plateforme et la distance avec le player, si celui-ci correspond au tag "MovingPlatform" et que le player est colle a la plateforme
+    // il set la plateformeen tant que parent du Player , si il saute de la plateforme le parent est set a null, Player redevient donc independant.
+                               //Utilisation
+    //associer ce script a un script de deplacement en appelant la fonction Move ci-dessous a chaque deplacement au lieu de le mettre dans une boucle update(comme ci-dessus) serait judicieux, Miguel out.
+    private void MovePlat()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(player.transform.position, Vector2.down);
+
+            if (hit.collider != null && hit.collider.tag == "MovingPlatform")
+            {
+                if (hit.distance < 1 && !isOnPlat)
+                {
+                    player.transform.SetParent(transform);
+                    isOnPlat = true;
+
+                }
+                if (hit.distance > 1 && isOnPlat)
+                {
+                    player.transform.SetParent(null);
+                    isOnPlat = false;
+                }
+            }
+            else
+            {
+                if (isOnPlat)
+                {
+                    player.transform.SetParent(null);
+                    isOnPlat = false;
+                }
+            }
+        }
+}
---
+++ boi/Assets/TimeCoins.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCoins : MonoBehaviour
+{
+    
+    public float timeBeforeVanishingInFrame;
+    public Transform hiddenPlace;
+    private float resetTimer;
+    public static bool isCharacterDead = false;
+    public Transform initialPosition;
+    public GameObject thisCoin;
+    public GameObject camera;
+    private bool firstEntrance = false;
+
+    void Start()
+    {
+        resetTimer = timeBeforeVanishingInFrame;
+        transform.position = initialPosition.transform.position;
+    }
+
+    void Update()
+    {
+        if (!camera.activeInHierarchy)
+           firstEntrance = true;
+        if (isCharacterDead == true || (camera.activeInHierarchy && firstEntrance == true))
+        {
+            firstEntrance = false;
+            timeBeforeVanishingInFrame = resetTimer;
+            transform.position = initialPosition.transform.position;    
+        }
+        if (isCharacterDead == false)
+            timeBeforeVanishingInFrame--;
+        if (timeBeforeVanishingInFrame <= 0)
+        {
+            transform.position = hiddenPlace.transform.position;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(thisCoin);
+        }
+    }
+
+        //   public void characterDead()
+        //   {
+        //       isCharacterDead = true;
+        //  }
+    }
